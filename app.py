723ece6e9b3f5b885b214b4af70aa2fd9281f51f
dolfius1er <charpentiernathan78@gmail.com>
2025-08-04 19:51:54 +0200

    Update and rename finaleapp.py to app.py

--- app.py
+++ app.py
@@ -50,3 +50,10 @@ def categories():
 
 if __name__ == '__main__':
     app.run(debug=True)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Render!"
